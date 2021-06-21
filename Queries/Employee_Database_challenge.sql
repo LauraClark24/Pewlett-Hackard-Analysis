7bdff387543d096fc4f4bed460f144a766a6943a
LauraClark24 <83182353+LauraClark24@users.noreply.github.com>
2021-06-20 19:18:33 -0500

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,36 @@
+Select e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+into retirement_titles
+from employees as e
+inner join titles as t
+on e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, from_date DESC;
+
+select * from unique_titles;
+select count(distinct title) from unique_titles;
+
+select count(emp_no) as number_retiring,  title
+INTO retiring_titles
+from unique_titles
+group by title
+order by count(emp_no) desc;
+
+select  DISTINCT ON (e.emp_no)e.emp_no,
+e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title 
+into mentorship_eligibilty
+from employees as e
+inner join titles as t
+on e.emp_no = t.emp_no
+inner join dept_emp as de
+on e.emp_no = de.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') and (de.to_date>='9999-01-01')
+order by e.emp_no;
+
+select * from mentorship_eligibilty;
